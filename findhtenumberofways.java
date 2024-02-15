5e5a6aa28efe10683041ab55faecb72e514de8fa
Nymur Khan Tonmoy <nktonmoy2230@gmail.com>
2024-02-15 23:36:32 +0530

    first commit

---
+++ findhtenumberofways.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class Solution {
+    public int numberOfPairs(ArrayList<ArrayList<Integer>> v) {
+        int n = v.size();
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            int x1 = v.get(i).get(0), y1 = v.get(i).get(1);
+
+            for (int j = 0; j < n; j++) {
+                if (i == j)
+                    continue;
+
+                int x2 = v.get(j).get(0), y2 = v.get(j).get(1);
+                int f = 0;
+
+                if (y1 >= y2 && x1 <= x2) {
+                    for (int k = 0; k < n; k++) {
+                        if (k == i || k == j)
+                            continue;
+
+                        int x3 = v.get(k).get(0), y3 = v.get(k).get(1);
+
+                        if (x3 >= x1 && x3 <= x2 && y3 >= y2 && y3 <= y1) {
+                            f = 1;
+                            break;
+                        }
+                    }
+
+                    if (f == 0)
+                        ans++;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
