3e39891a8e8703a5f16a6b0871a79ed232b3dcf8
Nymur Khan Tonmoy <nktonmoy2230@gmail.com>
2024-02-19 06:08:57 +0530

    done

---
+++ countbeautifulsubstring.java
@@ -0,0 +1,18 @@
+    public long beautifulSubstrings(String s, int k) {
+        int n = s.length(), v = 0, l;
+        for (l = 1; l * l % (K * 4) > 0; ++l);
+        Set<Character> vowels = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u'));
+        HashMap<Integer, Integer>[] seen = new HashMap[l];
+        for (int i = 0; i < l; i++) {
+            seen[i] = new HashMap<>();
+        }
+        seen[l - 1].put(0, 1);
+        long res = 0;
+        for (int i = 0; i < n; i++) {
+            v += vowels.contains(s.charAt(i)) ? 1: -1;
+            int c = seen[i % l].getOrDefault(v, 0);
+            res += c;
+            seen[i % l].put(v, c + 1);
+        }
+        return res;
+    }
