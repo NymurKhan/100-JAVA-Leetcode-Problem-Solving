2178bc2e8d7f55766d679710bb9d745914e943b4
Nymur Khan Tonmoy <nktonmoy2230@gmail.com>
2024-02-20 10:30:20 +0530

    done

---
+++ maxnmberofoperationii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxOperations(int[] nums) {
+        int n = nums.length;
+        int count = 1;
+        int score = nums[0] + nums[1];
+        for(int i = 2; i < n - 1; i++){
+            if(nums[i] + nums[i + 1] == score){
+                count++;
+                i++;
+            }
+            else{
+                break;
+            }
+        }
+        return count;
+    }
+}
